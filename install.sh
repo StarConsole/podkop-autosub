57dd9c56b39b3a9f032e326413f159deed11bf44
StarConsole <starconsoledev@gmail.com>
2026-07-30 14:12:04 +0300

    feat: 0.3.1

--- install.sh
+++ install.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
+# Определяем абсолютную директорию, где находится сам install.sh
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 echo "=== Установка podkop-autosub v0.3.0 ==="
 
 # 1. Создаем дефолтный конфиг, если его нет
 if [ ! -f /etc/config/podkop_rotator ]; then
     echo "[+] Копируем конфиг по умолчанию в /etc/config/podkop_rotator"
-    cp files/etc/config/podkop_rotator /etc/config/podkop_rotator
+    cp "$SCRIPT_DIR/files/etc/config/podkop_rotator" /etc/config/podkop_rotator
 else
     echo "[*] Конфиг /etc/config/podkop_rotator уже существует, сохраняем пользовательские настройки"
 fi
 
-# 2. Копируем исполняемые файлы и выставление прав
+# 2. Копируем исполняемые файлы и выставляем права
 echo "[+] Копируем скрипт и init.d сервис..."
-cp files/usr/bin/podkop-rotator.sh /usr/bin/podkop-rotator.sh
+cp "$SCRIPT_DIR/files/usr/bin/podkop-rotator.sh" /usr/bin/podkop-rotator.sh
 chmod +x /usr/bin/podkop-rotator.sh
 
-cp files/etc/init.d/podkop_rotator /etc/init.d/podkop_rotator
+cp "$SCRIPT_DIR/files/etc/init.d/podkop_rotator" /etc/init.d/podkop_rotator
 chmod +x /etc/init.d/podkop_rotator
 
 # 3. Включение службы в автозагрузку
